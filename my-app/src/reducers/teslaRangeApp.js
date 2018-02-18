719fe236edec24850d311051dfc78cec3f3128e6
oliwiah <oliwia.hajszprych@gmail.com>
2018-02-18 01:05:36 +0100

    app after 2 tests passed

--- my-app/src/reducers/teslaRangeApp.js
+++ my-app/src/reducers/teslaRangeApp.js
@@ -1,3 +1,5 @@
+import { getModelData } from '../services/BatteryService';
+
 const initialState = {
     carstats:[
         {miles:246, model:"60"},
@@ -15,9 +17,39 @@ const initialState = {
     }
 }
 
+function updateStats(state, newState) {
+    return {
+        ...state,
+        config:newState.config,
+        carstats:calculateStats(newState)
+    }
+}
+function calculateStats(state) {
+    const models = ['60', '60D', '75', '75D', '90D', 'P100D'];
+    const dataModels = getModelData();
+    return models.map(model => {
+        const { speed, temperature, climate, wheels } = state.config;
+        const miles = dataModels[model][wheels][climate ? 'on' : 'off'].speed[speed][temperature];
+        return {
+            model,
+            miles
+        };
+    });
+}
 function appReducer(state = initialState, action) {
     switch (action.type) {
-
+        case 'CHANGE_CLIMATE': {
+            const newState = {
+                ...state,
+                config: {
+                    climate: !state.config.climate,
+                    speed: state.config.speed,
+                    temperature: state.config.temperature,
+                    wheels: state.config.wheels
+                }
+            };
+            return updateStats(state, newState);
+        }
         default:
             return state
     }
